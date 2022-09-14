93374cc2f22c82fa078ba34e019fd1afaf6d9622
unknown <philipmodino@gmail.com>
2022-09-14 17:13:52 +0200

    Backend folder index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+
+app.listen(3000, ()=> {
+    console.log('server running');
+})
