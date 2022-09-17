3260d16f59422bc612d215790bdc85c292feed68
unknown <philipmodino@gmail.com>
2022-09-17 16:11:33 +0200

    Create Form

--- frontend/src/app/apiservice.service.ts
+++ frontend/src/app/apiservice.service.ts
@@ -17,5 +17,11 @@ export class ApiserviceService {
     {
       return this._http.get(`${this.apiUrl}`);
     }
+
+    // create data
+    createData(data:any):Observable<any>
+    {
+      return this._http.post(`${this.apiUrl}`,data);
+    }
     
 }
--- frontend/src/app/create/create.component.ts
+++ frontend/src/app/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-create',
@@ -12,4 +13,14 @@ export class CreateComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  userForm = new FormGroup({
+    'fullname':new FormControl('',Validators.required),
+    'email':new FormControl('',Validators.required),
+    'mobile':new FormControl('',Validators.required),
+  });
+
+  userSubmit()
+  {
+    console.log(this.userForm.value);
+  }
 }
