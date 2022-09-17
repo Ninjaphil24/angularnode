dae103525b94c006ae33d15b9d91f060a9a4eeeb
unknown <philipmodino@gmail.com>
2022-09-17 15:30:08 +0200

    Read Table

--- frontend/src/app/read/read.component.ts
+++ frontend/src/app/read/read.component.ts
@@ -9,9 +9,12 @@ export class ReadComponent implements OnInit {
 
   constructor(private service:ApiserviceService) { }
 
+  readData:any;
+
   ngOnInit(): void {
     this.service.getAllData().subscribe((res)=>{
       console.log(res,"res==>");
+      this.readData=res.data;
     });
   }
 
